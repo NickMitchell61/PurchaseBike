46ba0a14b43efb7f1ba424ca5847e642a4185e86
Maksym Nemera <maksym.nemera@outlook.com>
2022-08-31 12:36:40 +0300

    Added Action on the button which reaction how many bikes you can buy. Added try\catch exception and create a new exception called Too Many Bikes.

--- src/book/fain/yakov/purchaseBike.java
+++ src/book/fain/yakov/purchaseBike.java
@@ -1,6 +1,7 @@
 package book.fain.yakov;
 
 
+import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -15,6 +16,7 @@ public class purchaseBike extends JFrame implements ActionListener{
 	JTextField modelBike;
 	JTextField countBike;
 	JButton enterThePurchase;
+	JLabel text;
 	
 	public purchaseBike() {
 		
@@ -25,7 +27,7 @@ public class purchaseBike extends JFrame implements ActionListener{
 		
 		JPanel bottomPanel = new JPanel();
 		
-		JTextArea text = new JTextArea("");
+		text = new JLabel("");
 		text.setFont(font);
 		
 		enterThePurchase = new JButton("Замовити");
@@ -53,7 +55,7 @@ public class purchaseBike extends JFrame implements ActionListener{
 		JPanel p2 = new JPanel();
 		windowContent.add(p2);
 		
-		countBike = new JFormattedTextField("");
+		countBike = new JFormattedTextField(0);
 		countBike.setColumns(30);
 		countBike.setFont(font);
 		
@@ -62,27 +64,72 @@ public class purchaseBike extends JFrame implements ActionListener{
 		p2.add(lab2);
 		p2.add(countBike);
 		
-	
 		
 		setContentPane(windowContent);
-		setSize(600, 200);
+		setSize(800, 200);
 		setLocation(600, 200);
 		setTitle("Purchase Bike");
 		setVisible(true);
 		
 	}
-	
-
-	public static void main(String[] args) {		
-		new purchaseBike();
-	}
 
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
+		String selectedModel = modelBike.getText();
+		String selectedQuantity = countBike.getText();
+		
+		int quantity = Integer.parseInt(selectedQuantity);
+		
+		
+		try {
+			checkOrder(selectedModel, quantity);
+			text.setText("Ваше замовлення прийнято!");
+		} catch (TooManyBikesException e1) {
+			text.setText(e1.getMessage());
+		}
+		
+	}
+	
+	void checkOrder (String modelBike, int quantity) throws TooManyBikesException{
+		
+		if (modelBike.equals("Bike1") && quantity >= 3) {
+			throw new TooManyBikesException("Неможливо доставити " + quantity 
+					+ " велосипедів моделі " + modelBike + " за одну доставку.");
+		} else if (modelBike.equals("Bike2") && quantity >= 5) {
+			throw new TooManyBikesException("Неможливо доставити " + quantity 
+					+ " велосипедів моделі " + modelBike + " за одну доставку.");
+		} else if (modelBike.equals("Bike3") && quantity >= 10) {
+			throw new TooManyBikesException("Неможливо доставити " + quantity 
+					+ " велосипедів моделі " + modelBike + " за одну доставку.");
+		} else if (modelBike.equals("Bike4") && quantity >= 2) {
+			throw new TooManyBikesException("Неможливо доставити " + quantity 
+					+ " велосипедів моделі " + modelBike + " за одну доставку.");
+		}  else if (modelBike.equals("Bike5") && quantity >= 7) {
+			throw new TooManyBikesException("Неможливо доставити " + quantity 
+					+ " велосипедів моделі " + modelBike + " за одну доставку.");
+		} else if (modelBike.equals("")) {
+			throw new TooManyBikesException("Заповніть поле моделі");
+		} else if (quantity == 0) {
+			throw new TooManyBikesException("Заповніть поле кількості велосипедів.");
+		}
 		
 	}
 
 	
+	
+	public static void main(String[] args) {		
+		new purchaseBike();
+	}
+	
+}
+
+class TooManyBikesException extends Exception {
+	
+	public TooManyBikesException(String newExc) {
+		
+		super(newExc);
+		
+	}
 }
